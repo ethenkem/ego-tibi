188b100087758a8ad58785458ff07e4e5faafb44
ethenkem <ethenatx@gmail.com>
2026-06-26 19:14:50 +0000

    feat: add configuration class for initialization with commented-out runner bean

--- services/routingservice/src/main/java/com/egotibi/routingservice/modules/core/Init.java
+++ services/routingservice/src/main/java/com/egotibi/routingservice/modules/core/Init.java
@@ -1,5 +1,15 @@
 package com.egotibi.routingservice.modules.core;
 
-public class Init {
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+public class Init {
+    // @Bean
+    // CommandLineRunner runner() {
+    // return (args) -> {
+    // // Initialization logic here
+    // };
+    // }
 }
